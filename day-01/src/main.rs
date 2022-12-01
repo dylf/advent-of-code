0a90b53bb5b69857c3ef28e6289b270a3d8f1b5e
Dylan Fontaine <dylanfontaine@live.ca>
2022-12-01 14:09:20 -0800

    feat: day 1 problem 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -2,27 +2,18 @@ use std::{env, fs::File, io::Read};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
+
     let mut file = File::open(&args[1]).expect("Error opening file");
     let mut file_contents = String::new();
     file.read_to_string(&mut file_contents)
         .expect("Error reading line");
 
-    let mut max_elf_index = 0;
-    let mut max_elf_calories = 0;
-    let mut curr_total_calories = 0;
-    let mut curr_elf_index = 0;
-
     let mut calorie_totals = Vec::<i32>::new();
 
+    let mut curr_total_calories = 0;
     for line in file_contents.lines().into_iter() {
         if line.is_empty() {
-            if curr_total_calories > max_elf_calories {
-                max_elf_calories = curr_total_calories;
-                max_elf_index = curr_elf_index;
-            }
             calorie_totals.push(curr_total_calories);
-            curr_elf_index = curr_elf_index + 1;
             curr_total_calories = 0;
             continue;
         }
@@ -30,15 +21,18 @@ fn main() {
         curr_total_calories = curr_total_calories + line.parse::<i32>().unwrap();
     }
 
-    if curr_total_calories > max_elf_calories {
-        max_elf_calories = curr_total_calories;
-        max_elf_index = curr_elf_index;
-    }
+    // Push the final count after the loop
     calorie_totals.push(curr_total_calories);
 
+    calorie_totals.sort_by(|a, b| b.cmp(a));
+
     println!(
-        "Elf {} is carrying {} calories",
-        max_elf_index + 1,
-        max_elf_calories,
+        "Top elf is carrying {} calories",
+        calorie_totals.clone().first().unwrap(),
+    );
+
+    println!(
+        "Top 3 elves are carrying {} calories",
+        calorie_totals.clone().iter().take(3).sum::<i32>(),
     )
 }
