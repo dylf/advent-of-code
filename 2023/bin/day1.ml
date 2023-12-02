8aca6bdf3da8b5e10b614a36d3237bfe7554744b
Dylan Fontaine <10430358+dylf@users.noreply.github.com>
2023-12-02 09:34:20 -0800

    fix solution for both problems day1

--- 2023/bin/day1.ml
+++ 2023/bin/day1.ml
@@ -1,7 +1,5 @@
 open Core
 
-(* let noop x = x *)
-
 let filename =
   let args = Sys.get_argv () in
   Array.get args 1
@@ -41,33 +39,36 @@ let convert_digit d =
   | "nine" -> "9"
   | d -> d
 
-let find_digit line digit =
-  match String.substr_index line ~pattern:digit with
-  | None -> None
-  | Some pos -> Some (pos, digit)
+let noop x = x
 
-let compare_pos (x, _) (y, _) = compare x y
-let compare_pos_rev (x, _) (y, _) = compare y x
+let find_digit_occurrences line digit =
+  (String.substr_index_all line ~may_overlap:true ~pattern:digit, digit)
 
-let find_n_digit line f =
-  let found_digits = List.map digits ~f:(find_digit line) |> List.filter_opt in
-  let _, d = List.sort found_digits ~compare:f |> List.hd_exn in
+let min l = List.min_elt l ~compare:(fun x y -> compare x y) |> Option.value_exn
+let max l = List.max_elt l ~compare:(fun x y -> compare x y) |> Option.value_exn
+let cmp_first (x, _) (y, _) = compare (min x) (min y)
+let cmp_last (x, _) (y, _) = compare (max y) (max x)
+let filter_char_digits line = line |> String.filter ~f:Char.is_digit
+
+let find_n_digit line cmp =
+  let found_digits =
+    List.map digits ~f:(find_digit_occurrences line)
+    |> List.filter ~f:(fun (x, _) -> List.is_empty x |> not)
+  in
+  let _, d = List.sort found_digits ~compare:cmp |> List.hd_exn in
   convert_digit d
 
-let find_first_digit line = find_n_digit line compare_pos
-let find_last_digit line = find_n_digit line compare_pos_rev
-let filter_digits line = line |> String.filter ~f:Char.is_digit
+let find_first_digit line = find_n_digit line cmp_first
+let find_last_digit line = find_n_digit line cmp_last
+let first_and_last line = find_first_digit line ^ find_last_digit line
 
-let first_and_last line =
-  let first_digit = find_first_digit line in
-  let last_digit = find_last_digit line in
-  first_digit ^ last_digit
-
-let solve file =
+let solve file f =
   let contents = In_channel.read_all file in
   String.split_lines contents
-  |> List.map ~f:filter_digits |> List.map ~f:first_and_last
-  |> List.map ~f:Int.of_string |> List.fold ~init:0 ~f:( + )
+  |> List.map ~f |> List.map ~f:first_and_last |> List.map ~f:Int.of_string
+  |> List.fold ~init:0 ~f:( + )
 
 let () =
-  printf "Solution 1: %d\nSolution 2: %d\n" (solve filename) (solve filename)
+  printf "Solution 1: %d\nSolution 2: %d\n"
+    (solve filename filter_char_digits)
+    (solve filename noop)
