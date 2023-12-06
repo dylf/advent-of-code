da0d6480ca4847a31ded46c41e186c0c6bba5ec5
Dylan Fontaine <10430358+dylf@users.noreply.github.com>
2023-12-06 12:35:37 -0800

    solve part 2

--- 2023/bin/day06.ml
+++ 2023/bin/day06.ml
@@ -11,7 +11,9 @@ let int_list_from_string str =
   |> List.filter ~f:string_not_empty
   |> List.map ~f:Int.of_string
 
-let parse file =
+let int_from_string str = String.filter str ~f:Char.is_digit |> Int.of_string
+
+let parse_list file =
   let inputs =
     String.split_lines file
     |> List.filter ~f:string_not_empty
@@ -25,25 +27,41 @@ let parse file =
   in
   List.zip_exn time dist
 
-let find_ways_to_win (time, dist) =
-  List.init time ~f:(fun n -> n)
-  |> List.map ~f:(fun hold ->
-         let rem = time - hold in
-         if hold * rem > dist then true else false)
-  |> List.filter ~f:(fun b -> b)
-  |> List.length
+let parse_int file =
+  let inputs =
+    String.split_lines file
+    |> List.filter ~f:string_not_empty
+    |> List.map ~f:(fun s ->
+           let _, v = String.lsplit2_exn s ~on:':' in
+           v)
+    |> List.map ~f:int_from_string
+  in
+  match inputs with [ x; y ] -> [ (x, y) ] | _ -> [ (-1, -1) ]
+
+(* Brute force og *)
+(* let find_ways_to_win (time, dist) = *)
+(*   List.init time ~f:(fun n -> n) *)
+(*   |> List.filter ~f:(fun hold -> *)
+(*          let rem = time - hold in *)
+(*          if hold * rem > dist then true else false) *)
+(*   |> List.length *)
+
+(* a = 1 b = time c = dist *)
+let find_ways_to_win_but_smarter (time, dist) =
+  let discriminant = float_of_int ((time * time) - (4 * 1 * dist)) in
+  let sqrt_discriminant = sqrt discriminant in
+  let root1 = (-.float_of_int time +. sqrt_discriminant) /. 2.0 in
+  let root2 = (-.float_of_int time -. sqrt_discriminant) /. 2.0 in
+  (Float.round_down root1 |> int_of_float)
+  - (Float.round_up root2 |> int_of_float)
 
 let product l = List.fold l ~init:1 ~f:( * )
 
-(* let prnt_dbg l =  *)
-(*   List.map l ~f:(fun (t, d) -> printf "T:%d -> D%d" t d) *)
-(*   |> List.hd_exn *)
-
-(* let prnt_dbg l = List.map l ~f:(fun w -> printf "W:%d\n" w) |> List.hd_exn *)
-
-let solve file =
+let solve file ~parse =
   let contents = In_channel.read_all file in
-  parse contents |> List.map ~f:find_ways_to_win |> product
+  parse contents |> List.map ~f:find_ways_to_win_but_smarter |> product
 
-let () = printf "Solution 1: %d\n" (solve filename)
-(* printf "Solution 1: %d\nSolution 2: %d\n" *)
+let () =
+  printf "Solution 1: %d\nSolution 2: %d\n"
+    (solve filename ~parse:parse_list)
+    (solve filename ~parse:parse_int)
